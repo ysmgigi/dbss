2c2f3daacdbcbe76b0de993805580b413cb495dd
ysmgigi <ysmgigi@gmail.com>
2016-06-28 23:13:21 +0800

    add hash table code

--- include/hash_table.h
+++ include/hash_table.h
@@ -1,8 +1,24 @@
 #ifndef HASH_TABLE_H
 #define HASH_TABLE_H
 
+#include "rte_atomic.h"
+
 typedef struct {
-}hash_table;
+	pthread_spinlock_t lock;
+	void *data;
+}slot_t;
+
+typedef struct {
+	rte_atomic32_t size;					// size of hash table
+	rte_atomic32_t nel;						// number of elements in hash table
+	int (*key_cmp)(void *key1, void *key2); // key compare function
+	slot_t *slots;
+}ht_t;
+
+void ht_create(ht_t *ht, uint32_t size);
+void ht_destroy(ht_t *ht);
+int32_t ht_find(const char *key, int len);
+int32_t ht_insert(const char *key, int len, void *e);
 
 #endif
 
--- src/murmur_hash.c
+++ src/murmur_hash.c
@@ -52,3 +52,4 @@ uint32_t murmur3_32(const char *key, uint32_t len, uint32_t seed) {
 
 	return hash;
 }
+
