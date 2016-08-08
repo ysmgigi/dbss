58b2c0f479e0790682c4d6861c4baa5784c91255
ysmgigi <ysmgigi@gmail.com>
2016-08-08 23:11:25 +0800

    revise warning

--- src/rcv_pool.c
+++ src/rcv_pool.c
@@ -10,7 +10,7 @@ extern "C" {
 list_t rcv_pool;
 
 int init_rcv_pool() {
-	int i;
+	unsigned int i;
 	int nsz = sizeof(node_t);
 	int bsz = sizeof(rcv_buf_t);
 	list_create(&rcv_pool);
@@ -30,6 +30,7 @@ int init_rcv_pool() {
 		nd += nsz;
 		rbf += bsz;
 	}
+	return 0;
 }
 
 #ifdef __cplusplus
